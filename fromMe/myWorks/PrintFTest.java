6bb32e621694f6c527564653408493b98d815436
Hamdi Gülay <mhgulay@gmail.com>
2022-07-26 17:25:27 +0300

    Worked with printf

---
+++ fromMe/myWorks/PrintFTest.java
@@ -0,0 +1,12 @@
+package myWorks;
+
+public class PrintFTest {
+
+    public static void main(String[] args) {
+
+        String s = "Hello World";
+        System.out.printf("The String object %s is at hash code %h%n", s, s);
+
+    }
+
+}
